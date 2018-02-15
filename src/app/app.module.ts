128ea1903bfef509694f5282f33e86f7cdd55437
Alok Tripathi <alokt@damcogroup.com>
2018-02-15 10:55:30 +0530

    camera

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,23 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpModule } from '@angular/http';
+import { FileTransfer, FileUploadOptions, FileTransferObject } from '@ionic-native/file-transfer';
+import { File } from '@ionic-native/file';
+import { Camera } from '@ionic-native/camera';
 
 import { AuthInterceptor } from '../providers/auth.interceptor';
-
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { RegistrationPage } from '../pages/registration/registration';
 import { LoginPage } from '../pages/login/login';
 import { EditPage } from '../pages/editprofile/editprofile';
 import { MyleaguePage } from '../pages/myleague/myleague';
-import { MygroupPage} from '../pages/mygroup/mygroup'; 
+import { MygroupPage } from '../pages/mygroup/mygroup';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { RemoteServiceProvider } from '../providers/remote-service/remote-service';
-import { HttpModule } from '@angular/http';
 
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
@@ -69,6 +71,10 @@ export const environment = {
     SplashScreen,
     { provide: ErrorHandler, useClass: IonicErrorHandler },
     RemoteServiceProvider,
+    FileTransfer,
+    FileTransferObject,
+    File,
+    Camera,
     {
       provide: HTTP_INTERCEPTORS,
       useClass: AuthInterceptor,
