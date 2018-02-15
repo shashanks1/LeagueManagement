4fde955523dbc79b1b63a87fd73fe641c6a5421e
Rajatn07 <rajatnagpal07@gmail.com>
2018-02-15 17:58:27 +0530

    ui changes

--- src/pages/editprofile/editprofile.ts
+++ src/pages/editprofile/editprofile.ts
@@ -6,6 +6,9 @@ import { RemoteServiceProvider } from '../../providers/remote-service/remote-ser
 
 import { HomePage } from '../home/home';
 
+import { LoadingController, ToastController } from 'ionic-angular';
+import { FileTransfer, FileUploadOptions, FileTransferObject } from '@ionic-native/file-transfer';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 
 @Component({
     selector: 'edit-profile',
@@ -16,7 +19,13 @@ export class EditPage {
     successMessage: string;
     errorMessage: string;
 
-    constructor(private fb: FormBuilder, public navCtrl: NavController, public navParams: NavParams, public service: RemoteServiceProvider) {
+    imageURI:any;
+    imageFileName:any;
+
+    constructor(private fb: FormBuilder, public navCtrl: NavController, public navParams: NavParams, public service: RemoteServiceProvider,private transfer: FileTransfer,
+        private camera: Camera,
+        public loadingCtrl: LoadingController,
+        public toastCtrl: ToastController) {
 
         this.successMessage = '';
         this.errorMessage = '';
@@ -100,5 +109,64 @@ export class EditPage {
                 this.errorMessage = JSON.parse(this.errorMessage);
             });
     }
+
+
+    getImage() {
+        const options: CameraOptions = {
+          quality: 100,
+          destinationType: this.camera.DestinationType.FILE_URI,
+          sourceType: this.camera.PictureSourceType.PHOTOLIBRARY
+        }
+      
+        this.camera.getPicture(options).then((imageData) => {
+          this.imageURI = imageData;
+        }, (err) => {
+          console.log(err);
+          this.presentToast(err);
+        });
+      }
+
+
+      uploadFile() {
+        let loader = this.loadingCtrl.create({
+          content: "Uploading..."
+        });
+        loader.present();
+        const fileTransfer: FileTransferObject = this.transfer.create();
+      
+        let options: FileUploadOptions = {
+          fileKey: 'ionicfile',
+          fileName: 'ionicfile',
+          chunkedMode: false,
+          mimeType: "image/jpeg",
+          headers: {}
+        }
+      
+        fileTransfer.upload(this.imageURI, 'http://192.168.0.7:8080/api/uploadImage', options)
+          .then((data) => {
+          console.log(data+" Uploaded Successfully");
+          this.imageFileName = "http://192.168.0.7:8080/static/images/ionicfile.jpg"
+          loader.dismiss();
+          this.presentToast("Image uploaded successfully");
+        }, (err) => {
+          console.log(err);
+          loader.dismiss();
+          this.presentToast(err);
+        });
+      }
+
+      presentToast(msg) {
+        let toast = this.toastCtrl.create({
+          message: msg,
+          duration: 3000,
+          position: 'bottom'
+        });
+      
+        toast.onDidDismiss(() => {
+          console.log('Dismissed toast');
+        });
+      
+        toast.present();
+      }
 }
 
